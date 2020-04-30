061f782970114a92ea0cf99284f7f4ff7af3bda0
Jonathan Plona <jonathan.plona@gmail.com>
2020-04-30 11:00:37 -0700

    Add Grandma Mary's recipes, mostly converted

---
+++ recipes/Grandma Mary/convert.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+header='
+<head>
+<style>
+@media print {
+  @page {
+    size: 8in 6in;
+    margin: 0in;
+    padding: 0in;
+  } 
+  body {
+    padding: 0in;
+    margin: 0in;
+  }
+  article {
+    width: 6.5in;
+    height: 4.5in;
+    margin: 0.5in;
+    padding: .25in;
+  }
+}
+@media screen {
+  article {
+    margin-top: 1em;
+    padding: 1em;
+  }
+  body {
+    max-width: 8in;
+    margin: auto;
+  }
+}
+article {
+  max-width: 7in;
+  border-style: solid;
+}
+main {
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: space-around;
+  grid-column-gap: 2em;
+}
+dt::after {
+  content: ": ";
+}
+dl {
+  margin-left: 1em;
+  margin-right: 1em;
+  display: grid;
+  grid-template-columns: auto auto auto;
+  justify-content: space-between;
+}
+dt {
+  display: inline;
+}
+dd {
+  display: inline;
+  margin-inline-start: 0px;
+}
+h1 {
+  page-break-before: always;
+  margin-block-start: 0.0em;
+  margin-block-end: 0.5em;
+}
+h2 {
+  margin-block-start: 0.0em;
+  margin-block-end: 0.5em;
+}
+h3 {
+  margin-block-start: 0.5em;
+  margin-block-end: 0.5em;
+}
+h4 {
+  margin-block-start: 0.5em;
+  margin-block-end: 0.5em;
+}
+#subtitle {
+  margin-left: 1em;
+}
+</style>
+</head>
+'
+
+entries=''
+body=''
+find . -name "*.wdb" |
+  sort |
+  while read line; do
+    recipe="$(./convert_one.sh "${line}")"
+    entries="${entries}$(echo "${recipe}" | head -1)"
+    recipe_body="$(echo "${recipe}" | tail -n +2)"
+    echo "${header}${recipe_body}" > "${line%.wdb}.html"
+    body="${body}${recipe_body}"
+  done
+
+_last_category=''
+toc=$(echo "${entries}" |
+  sort -k 1,2 -t $'\t' |
+  while IFS=$'\t' read category recipe_name; do
+    if [[ "${category}" != "${_last_category}" && ! -z "${_last_category}" ]]; then
+      echo '<ol>'
+    fi
+  done)
+
+echo "${header}${body}" > recipes.html
+
+echo "${toc}"
---
+++ recipes/Grandma Mary/convert_one.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+recipe="$1"
+
+csv=$(unoconv -f csv --stdout "${recipe}")
+
+# Recipe Name,Category,Ingredient 1,...Ingredient 12,Instructions,Prep time,Servings,Author
+function extract() {
+  echo "${csv}" | csvtool col "$1" -
+}
+
+function cell() {
+  extract "$1" | csvtool format '%1' -
+}
+
+recipe_name=$(cell '1')
+category=$(cell '2')
+instructions=$(cell '15')
+prep_time=$(cell '16')
+servings=$(cell '17')
+author=$(cell '18')
+
+num_ingredients=$(extract '3-14' | csvtool trim r - | csvtool width -)
+
+last_ingredient="$(cell $(( 3 + ${num_ingredients} - 1 )) )"
+instructions="$(strings "${recipe}" | sed -n -e "/^ *${last_ingredient//\//\\\/} *\$/,/^ *${prep_time//\//\\\/} *\$/p" | tail -n +2 | head -n -1)"
+
+echo -e "${category}\t${recipe_name}"
+cat <<EOF
+<article>
+  <header>
+    <h1>${recipe_name}</h1>
+    <h4 id="subtitle">From the kitchen of ${author}</h4>
+    <dl id="metadata">
+      <div><dt>Category</dt><dd>${category}</dd></div>
+      <div><dt>Preparation time</dt><dd>${prep_time}</dd></div>
+      <div><dt>Servings</dt><dd>${servings}</dd></div>
+    </dl>
+  </header>
+  <hr/>
+  <main>
+  <section>
+    <h2>Ingredients</h2>
+    <ul>
+EOF
+
+for i in $(seq 3 1 $(( 2 + ${num_ingredients} )) ); do
+  echo "      <li>$(cell "$i")</li>"
+done
+
+cat <<EOF
+    </ul>
+  </section>
+  <section>
+    <h2>Instructions</h2>
+    <p>${instructions}</p>
+  </section>
+  </main>
+</article>
+EOF
